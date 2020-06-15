a6309c235c7c72096a911953c673fae78c654fa1
Nong Quang Huong <nqh.ictu@gmail.com>
2020-06-15 15:53:32 +0700

    qrcode show

--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
 import pyotp
+import qrcode
+import base64
 from odoo import models
 from odoo import fields
+from odoo import api
+from io import BytesIO
 
 
 class User(models.Model):
@@ -11,11 +15,32 @@ class User(models.Model):
         ('0', 'Disable')
     ],
         string='OTP Activate', default='0')
-    otp_key = fields.Char()
+    otp_key = fields.Char(string='OTP Key')
+    qr_code = fields.Binary(string="QR Code", attachment=True, store=True)
 
     def check_otp(self, otp):
         totp = pyotp.TOTP(self.otp_key)
-        print("\033[92m ------------------------- \033[0m")
-        print(totp.now())
-        print("\033[92m ------------------------- \033[0m")
         return totp.verify(otp)
+
+    @api.onchange('otp_status')
+    def generate_otp_key(self):
+        key = pyotp.random_base32()
+        for rec in self:
+            rec.otp_key = str(key)
+
+    @api.onchange('otp_key')
+    def generate_qr_code(self):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        data = pyotp.totp.TOTP(self.otp_key).provisioning_uri(self.email, issuer_name="Secure App")
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image()
+        temp = BytesIO()
+        img.save(temp, format="PNG")
+        qr_image = base64.b64encode(temp.getvalue())
+        self.qr_code = qr_image
